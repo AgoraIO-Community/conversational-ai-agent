65d69be3e9afe2667f1aab8cb539ef9d34aefe6b
Bhupendra Singh Negi <bhupendra1011@gmail.com>
2024-09-27 01:41:18 +0530

    Feature/video call (#11)
    * added ui controls
    
    * position controls at bottom
    
    * add user avatar
    
    * cam on by defult
    
    * added avatar
    
    * updated localcontrols
    
    * set muted
    
    * updated toggle state

--- frontend/app/agora/[channel]/page.tsx
+++ frontend/app/agora/[channel]/page.tsx
@@ -19,7 +19,7 @@ const AvatarUser = () => {
   );
 }
 
-const Userbadge = ({text}:{text:number|string}) =>{
+const Userbadge = ({ text }: { text: number | string }) => {
   return (<Badge variant="secondary" className="absolute bottom-3 right-3 p-2.5 border-0 z-[3]">{text}</Badge>)
 }
 
@@ -41,7 +41,7 @@ const RemoteUser: React.FC<{ user: IAgoraRTCRemoteUser, hasUserJoined: boolean }
       className='w-full h-full aspect-video border border-solid border-gray-300 rounded-lg overflow-hidden relative'
       id={`remote-user-${user.uid}`}
     >
-      <Userbadge text={user.uid}/>
+      <Userbadge text={user.uid} />
     </Card>
   );
 };
@@ -71,23 +71,23 @@ const App: React.FC = () => {
     redirect('/');
   }
 
-  const toggleMute = useCallback(() => {
+  const toggleMute = useCallback(async () => {
     if (localTracks[0]) {
       if (isMuted) {
-        localTracks[0].setEnabled(false);
+        await localTracks[0].setEnabled(true);
       } else {
-        localTracks[0].setEnabled(true);
+        await localTracks[0].setEnabled(false);
       }
       setIsMuted(!isMuted);
     }
   }, [localTracks, isMuted]);
 
-  const toggleCamera = useCallback(() => {
+  const toggleCamera = useCallback(async () => {
     if (localTracks[1]) {
       if (isCameraOn) {
-        localTracks[1].setEnabled(false);
+        await localTracks[1].setEnabled(false);
       } else {
-        localTracks[1].setEnabled(true);
+        await localTracks[1].setEnabled(true);
       }
       setIsCameraOn(!isCameraOn);
     }
@@ -102,7 +102,7 @@ const App: React.FC = () => {
     setIsCallActive(!isCallActive);
     // window.location.reload();
     router.push("/")
-    
+
   }, [isCallActive, localTracks]);
 
   const handleUserJoined = useCallback((user: IAgoraRTCRemoteUser) => {
@@ -224,15 +224,15 @@ const App: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className='self-center w-1/2 my-10'>
-      <div>
-      <div className="space-y-1">
-        <h4 className="text-lg font-medium leading-none">Agora Conversational AI</h4>
-        <p className="text-sm text-muted-foreground">
-          Participants: {users.length + 1}
-        </p>
-      </div>
-      <Separator className="my-4" />
-    </div>
+        <div>
+          <div className="space-y-1">
+            <h4 className="text-lg font-medium leading-none">Agora Conversational AI</h4>
+            <p className="text-sm text-muted-foreground">
+              Participants: {users.length + 1}
+            </p>
+          </div>
+          <Separator className="my-4" />
+        </div>
         {/* <Card>
           <CardContent>
               <h1>Agora Video Call</h1>
@@ -240,7 +240,7 @@ const App: React.FC = () => {
           </CardContent>
         </Card> */}
       </div>
-      
+
       <div className={`grid gap-10 ${users.length ? "grid-cols-2" : "grid-cols-1"
         } justify-center h-1/2 max-w-screen-lg m-auto`}>
         <div >
@@ -250,7 +250,7 @@ const App: React.FC = () => {
             id="localUser"
           >
             {!isCameraOn && <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center"><AvatarUser /></div>}
-            <Userbadge text={'Local User'}/>
+            <Userbadge text={'Local User'} />
           </Card>
           <div className="mt-auto  flex w-[300px] py-2 border-t  mx-auto justify-evenly items-center  rounded-[4px] my-5 ">
 
