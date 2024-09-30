3f58e1a8f3659bab34335551cd7bd1ce27e95d07
nitte93 <nitte.tiwari1993@gmail.com>
2024-09-30 08:52:19 +0530

    cors allow for all origin

--- frontend/app/api/proxy/route.ts
+++ frontend/app/api/proxy/route.ts
@@ -19,7 +19,7 @@ export async function OPTIONS(request: Request) {
     const response = new NextResponse(null, {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": allowedOrigin || "http://localhost:9000",
+        "Access-Control-Allow-Origin": allowedOrigin || "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers":
           "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
@@ -76,7 +76,7 @@ const allowedOrigin = request.headers.get("origin");
     const nextResponse = new NextResponse(data, {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": allowedOrigin || "http://localhost:9000",
+        "Access-Control-Allow-Origin": allowedOrigin || "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers":
           "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
@@ -93,7 +93,7 @@ const allowedOrigin = request.headers.get("origin");
     return new NextResponse(null, {
         status: 200,
         headers: {
-          "Access-Control-Allow-Origin": allowedOrigin || "http://localhost:9000",
+          "Access-Control-Allow-Origin": allowedOrigin || "*",
           "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
           "Access-Control-Allow-Headers":
             "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
